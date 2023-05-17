1e56767535a8648cf2d7fbfc37aaf5c953f57fb1
natalikoyani <natalikagami@gmail.com>
2023-05-17 17:24:19 +0200

    fix 3

--- js/task-06.js
+++ js/task-06.js
@@ -14,17 +14,41 @@ textInput.addEventListener('blur', onBlurChange)
 //     }
 // }
 
-const validValue = function () {
-    textInput.classList.add('valid')
-    textInput.classList.remove('invalid')
-}
+// -------------------------------------------------------
 
-const invalidValue = function () {
-    textInput.classList.add('valid')
-    textInput.classList.remove('invalid')
-}
+// const validValue = function () {
+//     textInput.classList.add('valid')
+//     textInput.classList.remove('invalid')
+// }
+
+// const invalidValue = function () {
+//     textInput.classList.add('valid')
+//     textInput.classList.remove('invalid')
+// }
+
+// function onBlurChange(event) {
+//     const currentLength = event.target.value.trim().length
+//     Number(validLength) === currentLength ? validValue : invalidValue
+// }
+
+// --------------------------------------------------------
+
+// function onBlurChange(event) {
+//     const currentLength = event.target.value.trim().length;
+//     const isValid = Number(validLength) === currentLength;
+//     setValidityClass(isValid);
+// }
+// function setValidityClass(isValid) {
+//     textInput.classList.toggle('valid', isValid);
+//     textInput.classList.toggle('invalid', !isValid);
+// }
+
+// --------------------------------------------------------
 
 function onBlurChange(event) {
-    const currentLength = event.target.value.trim().length
-    Number(validLength) === currentLength ? validValue : invalidValue
+    const currentLength = event.target.value.trim().length;
+    const isValid = Number(validLength) === currentLength;
+    textInput.classList.remove('valid');
+    textInput.classList.remove('invalid');
+    textInput.classList.add(isValid ? 'valid' : 'invalid');
 }
--- js/task-07.js
+++ js/task-07.js
@@ -3,11 +3,11 @@ const outputText = document.getElementById('text')
 
 inputRange.addEventListener('input', onRangeChange)
 
+outputText.style.fontSize = `${inputRange.value}px`
+
 function onRangeChange(event) {
     outputText.style.fontSize = `${event.currentTarget.value}px`
 }
-
-
 
 
 
