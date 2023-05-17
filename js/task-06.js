3046336c44ec354237419f5f906976a8997b7d5a
natalikoyani <natalikagami@gmail.com>
2023-05-17 16:23:44 +0200

    fix 3

--- js/task-06.js
+++ js/task-06.js
@@ -3,13 +3,28 @@ const validLength = textInput.getAttribute('data-length')
 
 textInput.addEventListener('blur', onBlurChange)
 
+// function onBlurChange(event) {
+//     const currentLength = event.target.value.trim().length
+//     if (Number(validLength) === currentLength) {
+//         textInput.classList.add('valid')
+//         textInput.classList.remove('invalid')
+//     } else {
+//         textInput.classList.remove('valid')
+//         textInput.classList.add('invalid')
+//     }
+// }
+
+const validValue = function () {
+    textInput.classList.add('valid')
+    textInput.classList.remove('invalid')
+}
+
+const invalidValue = function () {
+    textInput.classList.add('valid')
+    textInput.classList.remove('invalid')
+}
+
 function onBlurChange(event) {
     const currentLength = event.target.value.trim().length
-    if (Number(validLength) === currentLength) {
-        textInput.classList.add('valid')
-        textInput.classList.remove('invalid')
-    } else {
-        textInput.classList.remove('valid')
-        textInput.classList.add('invalid')
-    }
+    Number(validLength) === currentLength ? validValue : invalidValue
 }
