590d94da784878df774dbb19c7b2ccab1ce43dd0
natalikoyani <natalikagami@gmail.com>
2023-06-20 00:38:25 +0200

    finish

--- js/task-06.js
+++ js/task-06.js
@@ -1,29 +1,15 @@
-// const textInput = document.getElementById('validation-input')
-// const validLength = textInput.getAttribute('data-length')
+const textInput = document.getElementById('validation-input')
+const validLength = textInput.getAttribute('data-length')
 
-// textInput.addEventListener('blur', onBlurChange)
+textInput.addEventListener('blur', onBlurChange)
 
-// function onBlurChange(event) {
-//     const currentLength = event.target.value.trim().length
-//     if (Number(validLength) === currentLength) {
-//         textInput.classList.add('valid')
-//         textInput.classList.remove('invalid')
-//     } else {
-//         textInput.classList.remove('valid')
-//         textInput.classList.add('invalid')
-//     }
-// }
-
-
-
-const inputTextEl = document.getElementById("validation-input");
-
-inputTextEl.addEventListener("blur", inputValidation);
-
-function inputValidation(event) {
-    const dataLength = inputTextEl.getAttribute("data-length");
-    const valueLength = event.target.value.trim().length;
-    Number(dataLength) === valueLength
-        ? inputTextEl.classList.add("valid")
-        : inputTextEl.classList.add("invalid");
+function onBlurChange(event) {
+    const currentLength = event.target.value.trim().length
+    if (Number(validLength) === currentLength) {
+        textInput.classList.add('valid')
+        textInput.classList.remove('invalid')
+    } else {
+        textInput.classList.remove('valid')
+        textInput.classList.add('invalid')
+    }
 }
